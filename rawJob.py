8075fa0e36ecbc219b7dca2bc5a872b4e27d5841
Alec <alec.radford@gmail.com>
2013-02-23 14:13:55 -0500

    Fixing csv parse issue - semi-hackily

--- rawJob.py
+++ rawJob.py
@@ -1,8 +1,16 @@
+import re
+# Slightly modified from Stack Overflow
+parsePattern = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
 class rawJob:
 	'''This is a class for interacting directly with the data provided
 	by the competition csv file'''
 	def __init__(self, rawString):
-		values = [string.strip() for string in rawString.split(",")]
+		values = [string.strip() for string in parsePattern.split(rawString)]
+		# Clean up since I don't know regex well enough
+		values = [val for val in values if val != ',' and val != ' ' and val != '']
+		for i,value in enumerate(values):
+			if value == ',,':
+				values[i] = None
 		categories = ["id", "title", "description", "rawLocation", "normalizedLocation",
 						"contractType", "contractTime", "company", "category",
 						"salaryRaw", "salaryNormalized","sourceName"]
