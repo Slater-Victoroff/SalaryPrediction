c3c4a811530c267fb0f14b13d583393614c880c1
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-03-25 03:22:43 -0400

    Java time, the gloves are off

---
+++ java/code/salaryPredictionRBM/src/moneyMaker/JobList.java
@@ -0,0 +1,15 @@
+package moneyMaker;
+
+import java.util.List;
+
+
+public class JobList {
+
+	private List<Job> allJobs;
+	private static final String FULL_DATA="/home/svictoroff/Documents/Kaggle/SalaryPrection/data/Train_rev1.csv";
+	private static final String SAMPLE_DATA="/home/svictoroff/Documents/Kaggle/SalaryPrection/data/Train_rev1_sample.csv";
+	private static final String SMALL_SAMPLE_DATA="/home/svictoroff/Documents/Kaggle/SalaryPrection/data/Train_rev1_small_sample.csv";
+	private static final String TINY_SAMPLE_DATA="/home/svictoroff/Documents/Kaggle/SalaryPrection/data/Train_rev1_tiny_sample.csv";
+	private static final String TEST_DATA="/home/svictoroff/Documents/Kaggle/SalaryPrection/data/Train_rev1.csv";
+	
+}
